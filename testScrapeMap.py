ab334a4bcea4000f840094215acecffd099c05c6
yuniar rahmatika <71185845+yuniarrahmatika@users.noreply.github.com>
2021-07-15 17:11:03 +0700

    membuat file scrapping google maps with selenium

---
+++ testScrapeMap.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import re #regular exp
+import unicodecsv as csv 
+import time
+driver = webdriver.Chrome(executable_path="C:/Users/yuniar tika/Downloads/chromedriver_win32/chromedriver.exe")
+driver.get("https://www.google.com/maps/place/Jl.+Malioboro,+Sosromenduran,+Gedong+Tengen,+Kota+Yogyakarta,+Daerah+Istimewa+Yogyakarta/@-7.7926306,110.3636555,17z/data=!4m7!3m6!1s0x2e7a5825fa6106c5:0x3ea4c521a5ed1133!8m2!3d-7.7926306!4d110.3658442!9m1!1b1")
+time.sleep(5)
+reviews = driver.find_elements_by_xpath("//button[@class='widget-pane-link']")
+driver.execute_script(
+    'var d = document.getElementsByClassName("section-layout section-scrollbox scrollable-y scrollable-show")[0];'
+    'function sleep(ms){return new Promise(resolve => setTimeout(resolve, ms));}'+
+    'var comment = document.getElementsByClassName("section-review-review-content");'+
+    'async function scrolldown(){while(comment.length < 10){d.scrollTop += 10;await sleep(100)}}'+
+    'scrolldown()'
+)
+time.sleep(5)
+comment_text = driver.find_elements_by_xpath(".//div[@class='section-review-content']")
+def writeTofile(data):
+    with open('datawisataxxx.csv', mode='ab') as csv_file:
+        fieldnames =['id', 'user','komentar']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames,lineterminator='\n')
+        writer.writerow(data)
+for komen in comment_text:
+    section = komen.find_element_by_xpath(".//div[@class='section-review-title']")
+    id_r = section.find_element_by_xpath(".//span").text
+    #bintang = komen.find_element_by_xpath(".//span[@class='section-review-stars']").get_attribute('aria-label')
+    komentar = komen.find_element_by_xpath(".//span[@class='section-review-text']").text
+    #r= re.search(r'\d+', bintang)
+    #bintang = r.group(0) if r else ""
+    data = {'id':20,'user':id_r,'komentar': komentar}
+    print(data)
+    writeTofile(data)
